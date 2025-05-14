ab8f23a5f23e88503991cada4876dbb96bcfc708
shinjith-dev <shinjithpallikkara@gmail.com>
2025-05-15 00:00:25 +0530

    feat: add theme config
    Signed-off-by: shinjith-dev <shinjithpallikkara@gmail.com>

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,14 @@
+import { THEME } from "@/lib";
 import { Stack } from "expo-router";
+import { useColorScheme } from "react-native";
 
 export default function RootLayout() {
-  return <Stack />;
+  const theme = useColorScheme()
+
+  return <Stack
+    screenOptions={{
+      headerStyle: { backgroundColor: THEME.color[theme].base },
+      headerTitleStyle: { color: THEME.color[theme].foreground }
+    }}
+  />;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,23 @@
-import { Text, View } from "react-native";
+import { THEME } from "@/lib";
+import { Text, useColorScheme, View } from "react-native";
 
 export default function Index() {
+  const theme = useColorScheme()
+
   return (
     <View
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        backgroundColor: THEME.color[theme].background
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text
+        style={{
+          color: THEME.color[theme].foreground
+        }}
+      >Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
